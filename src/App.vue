b634a6624985ce0aafb64f4141599db11b8f6c5b
Sheila Jimenez Morejon <jimenezh@bu.edu>
2019-11-03 11:22:50 -0500

    got rid of navbar and date picker

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <v-app>
-    <navigationBar />
+    <!-- <navigationBar /> -->
     <router-view />
   </v-app>
 </template>
 
 <script>
-import navigationBar from "@/components/NavigationBar.vue";
+// import navigationBar from "@/components/NavigationBar.vue";
 export default {
   name: 'App',
   components: {
-    navigationBar,
+    // navigationBar,
   },
   data: () => ({
     //
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,13 +17,13 @@
       </popUp>
       <popUp>
         <template v-slot:buttonText>Schedule a Call</template>
-        <template v-slot:title>Schedule a Call</template>
+        <template v-slot:title><h1>Schedule a Call</h1></template>
         <template v-slot:content>
           <v-select  :items="contacts" label="Select a person from your contacts"></v-select>
 
-          <v-row justify="center">
+          <!-- <v-row justify="center">
               <v-date-picker v-model='calendarDate'></v-date-picker>
-          </v-row>
+          </v-row> -->
           <v-row justify="center">
               <v-time-picker v-model="calendarTime"></v-time-picker>
           </v-row>
