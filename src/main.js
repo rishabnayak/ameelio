68a81f345951a994e6502f53c52602aca5cc9d94
Shreya Banga <shreya.banga@gmail.com>
2019-11-06 17:07:06 -0500

    Fixed double firebase initialization
    removed the initialization from main.js

--- src/main.js
+++ src/main.js
@@ -23,16 +23,5 @@ firebase.auth().onAuthStateChanged(async () => {
   }
 });
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBDN5UQB9hl_z_Nc6N3yC_dnnsCbJ-7tvs",
-  authDomain: "ameelio-badeb.firebaseapp.com",
-  databaseURL: "https://ameelio-badeb.firebaseio.com",
-  projectId: "ameelio-badeb",
-  storageBucket: "ameelio-badeb.appspot.com",
-  messagingSenderId: "42596678946",
-  appId: "1:42596678946:web:b20fab397e8d62ffa7797b",
-  measurementId: "G-0GE5WZXYKH"
-});
-
 export const db = firebase.firestore();
 
