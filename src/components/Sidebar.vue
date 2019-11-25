f399a45f3c9e3ea650e1aab2aeba4829c1d4e94e
Sheila Jimenez Morejon <jimenezh@bu.edu>
2019-11-25 15:51:37 -0500

    used user() to add conditional rendering

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,47 +1,50 @@
 <template>
-  <nav>
-    <v-navigation-drawer id="drawer" app dark class="blue" :mini-variant.sync="mini" permanent>
-      <v-img v-if="!mini" src="@/assets/logo.png" height="40" contain @click="home" />
-      <v-img v-else src="@/assets/logo_mini.png" height="35" contain @click="home" />
-      <v-divider v-if="!mini"></v-divider>
-      <v-list-item v-if="!mini" @click="profile">
-        <v-list-item-avatar size="100">
-          <v-img :src="photoURL"></v-img>
-        </v-list-item-avatar>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-avatar v-if="mini">
-          <v-img :src="photoURL"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title><h1>{{name}}</h1></v-list-item-title>
-        </v-list-item-content>
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
-        >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-      <template v-slot:append>
-        <v-list dense>
-          <v-list-item>
-            <div v-if="!mini">
-              <v-btn block color="error" @click="signOut()">Logout</v-btn>
-            </div>
-            <div v-else>
-              <v-btn icon>
-                <v-icon @click="signOut()">power</v-icon>
-              </v-btn>
-            </div>
-          </v-list-item>
-        </v-list>
-      </template>
-    </v-navigation-drawer>
-  </nav>
+  <div v-if="user">
+    <nav>
+      <v-navigation-drawer id="drawer" app dark class="blue" :mini-variant.sync="mini" permanent>
+        <v-img v-if="!mini" src="@/assets/logo.png" height="40" contain @click="home" />
+        <v-img v-else src="@/assets/logo_mini.png" height="35" contain @click="home" />
+        <v-divider v-if="!mini"></v-divider>
+        <v-list-item v-if="!mini" @click="profile">
+          <v-list-item-avatar size="100">
+            <v-img :src="photoURL"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-avatar v-if="mini">
+            <v-img :src="photoURL"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title><h1>{{name}}</h1></v-list-item-title>
+          </v-list-item-content>
+          <v-btn
+            icon
+            @click.stop="mini = !mini"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <template v-slot:append>
+          <v-list dense>
+            <v-list-item>
+              <div v-if="!mini">
+                <v-btn block color="error" @click="signOut()">Logout</v-btn>
+              </div>
+              <div v-else>
+                <v-btn icon>
+                  <v-icon @click="signOut()">power</v-icon>
+                </v-btn>
+              </div>
+            </v-list-item>
+          </v-list>
+        </template>
+      </v-navigation-drawer>
+    </nav>
+  </div>
 </template>
 <script>
 import { log } from "util";
+import firebase from '../../firebase'
 export default {
   name: "Sidebar",
   computed: {
@@ -64,9 +67,6 @@ export default {
   methods: {
     async signOut() {
       await this.$store.dispatch("logOut");
-      this.$router.push("/login");
-    },
-    signIn() {
       this.$router.push("/login");
     },
     home() {
