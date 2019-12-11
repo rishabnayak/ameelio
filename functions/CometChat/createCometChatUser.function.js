093e03c226c8ba3b1324af8430e1b445112eb9ab
Sheila Jimenez Morejon <jimenezh@bu.edu>
2019-12-11 15:26:36 -0500

    maybe working cometchat user creation

--- functions/CometChat/createCometChatUser.function.js
+++ functions/CometChat/createCometChatUser.function.js
@@ -31,23 +31,8 @@ module.exports.createCometChatUser = functions.auth.user().onCreate((user) => {
         },
         body: JSON.stringify(body)
     };
-    // these are the options to create the authtoken for the user
-    var options1 = {
-        method: 'POST',
-        url: `https://api-us.cometchat.io/v2.0/users/${user.uid}/auth_tokens`,
-        headers: {
-            appid: '11033fd257dda26',
-            apikey: '666dfb04a6f6d3b31ba2c535c0a3f59570c7ecb7',
-            'content-type': 'application/json',
-            accept: 'application/json'
-        }
-    };
 
-    rp(options).then(() => {
-        rp(options1).then((data) => {
-            db.collection("users").doc(user.uid).update({ authToken: JSON.parse(data).data.authToken })
-        });
-    });
+    rp(options);
 
     return true;
 });
